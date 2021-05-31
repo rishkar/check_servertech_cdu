2f35695508e2ecb30decfe0f16c3ca9082c40534
Rishabh Karanth <me@karanth.dev>
2021-05-30 23:28:49 -0400

    Add preflight checks to verify SNMP hostname/community string

--- check_servertech_cdu.py
+++ check_servertech_cdu.py
@@ -2,6 +2,7 @@
 
 import argparse
 import easysnmp
+import sys
 
 # Define parser and arguments
 parser = argparse.ArgumentParser(description='This script runs SNMPv2c checks' 
@@ -37,3 +38,24 @@ snmp_crit_temp = args.temp
 snmp_conn_timeout = args.timeout
 be_verbose = args.verbose
 be_debug = args.debug
+
+# Try to establish a connection with the SNMP host and grab a basic variable.
+# If this fails, exit immedeately.
+
+# Test hostname
+try:
+    snmp_connection = easysnmp.Session(hostname=snmp_host, version=2, 
+                                    community=snmp_community,
+                                    timeout=snmp_conn_timeout)
+except easysnmp.exceptions.EasySNMPConnectionError:
+    print("ERROR: Could not establish SNMP connection with the host. Is your"
+        " hostname correct?")
+    sys.exit(1)
+
+# Test SNMP community string
+try:
+    snmp_connection.get("sysDescr.0")
+except easysnmp.exceptions.EasySNMPTimeoutError:
+    print("ERROR: Timed out while attempting to communicate with the host. Is"
+        " your SNMP community string correct?")
+    sys.exit(1)
